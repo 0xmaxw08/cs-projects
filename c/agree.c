48c3d121a38cfa52cf67e3fbd80afec617d7ac16
0xmaxw08 <maxw08@proton.me>
2026-07-02 17:55:06 +0000

    Updated agree.c

--- c/agree.c
+++ c/agree.c
@@ -3,16 +3,20 @@
 int main(void){
     char x;
 
-    printf("Do you agree? ");
-    scanf("%c", &x);
-
-    if (x == 'y' || x == 'Y'){ // || works as or
-        printf("Agreed :)\n");
-    }
-    else if (x == 'n' || x == 'N'){
-        printf("Not agreed :(\n");
-    }
-    else{
-        printf("Invalid Input!\n");
+    while(1){
+        printf("Do you agree? ");
+        scanf(" %c", &x);
+    
+        if (x == 'y' || x == 'Y'){ // || works as or
+            printf("Agreed :)\n");
+            break;
+        }
+        else if (x == 'n' || x == 'N'){
+            printf("Not agreed :(\n");
+            break;
+        }
+        else{
+            printf("Invalid Input!\n");
+        }
     }
 }
