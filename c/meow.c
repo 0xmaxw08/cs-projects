aadb73e55d23aeee83cbab01c43ed0ff85836caf
0xmaxw08 <maxw08@proton.me>
2026-07-05 05:44:46 +0000

    Updated meow.c

--- c/meow.c
+++ c/meow.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
+# include <stdio.h>
+# include <stdbool.h>
 
 int main(void){
-    int i = 3;
-    while (i > 0){
-        printf("meoow\n");
-        i--;
+    int n;
+
+    while (true){
+        printf("Number of repetitions? ");
+        scanf("%d", &n);
+
+        if (n>=0){
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        printf("meow\n");
     }
 }
