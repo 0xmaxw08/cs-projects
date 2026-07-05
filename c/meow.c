407d5a538db857d512d32b7d459ca161c27dd0f3
0xmaxw08 <maxw08@proton.me>
2026-07-05 10:39:35 +0000

    Updated meow.c

--- c/meow.c
+++ c/meow.c
@@ -4,15 +4,27 @@
 int main(void){
     int n;
 
-    while (true){
-        printf("Number of repetitions? ");
-        scanf("%d", &n);
+    do {
+        printf("Enter number of repetitions: ");
 
-        if (n>=0){
-            break;
+        if (scanf("%d", &n) != 1) {
+            printf("Invalid Input. Try again. \n");
+            n = -1;
+            while (getchar() != '\n');
         }
-    }
+    } while(n<0);
+
+    
+    // while (true){
+    //     printf("Number of repetitions? ");
+    //     scanf("%d", &n);
+
+    //     if (n>=0){
+    //         break;
+    //     }
+    // }
     for (int i = 0; i < n; i++){
         printf("meow\n");
     }
+    return 0;
 }
